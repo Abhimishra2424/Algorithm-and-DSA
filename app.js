d4d084acce8e41d4f0b1e07fcf981b6e4a1f78c6
Abhishek <abmishra700@gmail.com>
2022-06-10 18:25:56 +0530

    isPowerOfTwoBitWise

--- app.js
+++ app.js
@@ -118,17 +118,28 @@
 
 // Bi-O  O(sqrt(n)) linnear time complexity
 
-const powerOfTwo = (n) => {
-  if (n < 1) {
-    return false;
-  }
-  while (n > 1) {
-    if (n % 2 !== 0) {
-      return false;
-    }
-    n = n / 2;
-  }
-  return true;
-};
+// const powerOfTwo = (n) => {
+//   if (n < 1) {
+//     return false;
+//   }
+//   while (n > 1) {
+//     if (n % 2 !== 0) {
+//       return false;
+//     }
+//     n = n / 2;
+//   }
+//   return true;
+// };
 
-console.log(powerOfTwo(5));
+// console.log(powerOfTwo(5));
+
+//  >> Big O notation = Big O(logn)
+
+// function isPowerOfTwoBitWise(n) {
+//   if (n < 1) {
+//     return false;
+//   }
+//   return (n & (n - 1)) === 0;
+// }
+
+// Big o = o(1)
