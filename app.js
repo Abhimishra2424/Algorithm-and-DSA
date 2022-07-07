24dd8e13f8b78d9a25731156873062f458bbef18
Abhishek Mishra <abmishra700@gmail.com>
2022-07-07 11:23:00 +0530

    binarySearch

--- app.js
+++ app.js
@@ -158,14 +158,41 @@
 
 //  Linear search
 
-function linearSeacrh(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
-    }
-  }
-  return -1;
-}
+// function linearSeacrh(arr, target) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === target) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
 
 // Big O = O(n)
-console.log(linearSeacrh([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 5)); // 4
+// console.log(linearSeacrh([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 5)); // 4
+
+// Binary search
+
+// function binarySearch(sortedArray, key) {
+//   let start = 0;
+//   let end = sortedArray.length - 1;
+
+//   while (start <= end) {
+//     let middle = Math.floor((start + end) / 2);
+
+//     if (sortedArray[middle] === key) {
+//       // found the key
+//       return middle;
+//     } else if (sortedArray[middle] < key) {
+//       // continue searching to the right
+//       start = middle + 1;
+//     } else {
+//       // search searching to the left
+//       end = middle - 1;
+//     }
+//   }
+//   // key wasn't found
+//   return -1;
+// }
+// Big O =  O(logn)
+// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 5)); //  index 4
+
